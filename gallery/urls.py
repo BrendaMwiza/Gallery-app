4c4d40a6e4ae1135b4ac6e791a36ba10ead862ac
BrendaMwiza <brendabrizy@gmail.com>
2019-10-10 14:35:13 +0200

    added view function past_days

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.todays_pic,name='picToday')
+    url('^today/$',views.todays_pic,name='picToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_pics,name = 'pastPics') 
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
+from django.http  import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
@@ -12,19 +13,33 @@ def todays_pic(request):
     html = f'''
         <html>
             <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>            
+                <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>            
             </body>
         </html>
             '''
     return HttpResponse(html)
 
-def convert_dates(dates):
+def past_days_pics(request, past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
 
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
+    if date == dt.date.today():
+        return redirect(pics_today)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+    photo = Category.days_photo(date)
+    return render(request, 'all-photos/past-phots.html',{"date": date,"photo":photo})
 
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
